7408cbbc1813b62c22c8d451b024a96ae44f34f8
madurangaPrabhath <139701327+madurangaPrabhath@users.noreply.github.com>
2024-11-24 13:33:58 +0000

    Added useEffect

--- assignment4/src/components/AnimalTable.js
+++ assignment4/src/components/AnimalTable.js
@@ -1,25 +1,33 @@
 import { animals } from '../assets/data/AnimalsDb';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function AnimalTable(props) {
-    const [randomAnimal, setRandomAnimal] = useState('');
-    const [selectedAnimal, setSelectedAnimal] = useState('');
+    const [randomAnimal, setRandomAnimal] = useState(null);
     const [result, setResult] = useState('');
 
+    useEffect(() => {
+        const randomIndex = Math.floor(Math.random() * animals.length);
+        setRandomAnimal(animals[randomIndex]);
+    }, []);
+
+
     const handleAnimalClick = (selectedAnimal) => {
-        if (selectedAnimal === randomAnimal) {
+        if (selectedAnimal === randomAnimal.name) {
             setResult('WIN');
         } else {
             setResult('LOSE');
         }
     };
 
+    if (!randomAnimal) {
+        return <div className="loading">Loading...</div>;
+    }
     return (
         <div className='gameContainer'>
             <table className='game-table'>
                 <thead>
                     <tr >
-                        <th colspan="3">Animal Matching Game</th>
+                        <th colSpan="3">Animal Matching Game</th>
                     </tr>
                     <tr>
                         <th>Result</th>
@@ -30,10 +38,10 @@ export default function AnimalTable(props) {
                 <tbody>
                     <tr>
                         <td className='result-col'>
-                            <h2>Result</h2></td>
+                            <h2>{result}</h2></td>
 
                         <td className='animalname-col'>
-                            <h2>{randomAnimal.toUpperCase()}</h2></td>
+                            <h2>{randomAnimal.name.toUpperCase()}</h2></td>
                         <td className='animalgrid-col'>
                             <div>
                                 <div className='animalgrid'>
