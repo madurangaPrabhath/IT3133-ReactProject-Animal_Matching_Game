2d9a76adc27b6035745ade82448ebeffe81d374c
madurangaPrabhath <139701327+madurangaPrabhath@users.noreply.github.com>
2024-11-24 13:12:51 +0000

    Adding table layout

--- assignment4/src/components/AnimalTable.js
+++ assignment4/src/components/AnimalTable.js
@@ -1,7 +1,32 @@
-export default function AnimalTable() {
-    return(
-        <div>
-            
+import { animals } from '../assets/data/AnimalsDb';
+
+export default function AnimalTable(props) {
+    return (
+        <div className='gameContainer'>
+            <table className='game-table'>
+                <thead>
+                    <tr >
+                        <th colspan="3">Animal Matching Game</th>
+                    </tr>
+                    <tr>
+                        <td >Result</td>
+                        <td>Animal Name</td>
+                        <td>Select the Animal</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><h2>Result</h2></td>
+
+                        <td><h2>Animal Name</h2></td>
+                        <td>
+                            <div>
+
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
